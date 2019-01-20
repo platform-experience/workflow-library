250e9ec15eec584c65f685bd0650f5e0585d0a75
Steven Gregory <sgregory@gmail.com>
2019-01-20 13:06:51 -0600

    Added shell automation scripts

---
+++ scripts/config.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# directories
+IMAGE_DIR=images
+SRC_DIR=src
+
+# extensions
+UPDATE_SET=u-update-set.xml
+
+# gists
+CONFIG_GIST=https://gist.githubusercontent.com/stevengregory/a705d21dc89f191101e247183050754b/raw/0fc880812e1820b5faa43be383be2eea09a9eb4c/config.json
+README_GIST=https://gist.githubusercontent.com/stevengregory/c092fbfeddd27e46a1f9f263644652b7/raw/7cec6ef3b6e8015781bf0196ac2cc5450e39020a/README.md
+
+# prefix
+PREFIX=wf
+
+# temp content
+CONTRIB_TEMP='Sush Chandrashekar'
+DIR_TEMP='temp-name'
+NAME_TEMP='Temp Name'
